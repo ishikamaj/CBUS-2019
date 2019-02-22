f7eeca297dbc3e6523867254ffde7f43404b3d95
Ishika Majumder <36339352+ishikamaj@users.noreply.github.com>
2019-02-21 19:10:35 -0500

    tips files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'water-tips', loadChildren: './water-tips/water-tips.module#WaterTipsPageModule' },
+  { path: 'heating-tips', loadChildren: './heating-tips/heating-tips.module#HeatingTipsPageModule' },
+  { path: 'lighting-tips', loadChildren: './lighting-tips/lighting-tips.module#LightingTipsPageModule' },
+  { path: 'air-tips', loadChildren: './air-tips/air-tips.module#AirTipsPageModule' },
+  { path: 'tech-tips', loadChildren: './tech-tips/tech-tips.module#TechTipsPageModule' }
 ];
 @NgModule({
   imports: [
